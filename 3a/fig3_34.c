254a8cd9dfc944dd3df2b53abc228dade8dc5488
Sushant Aryal <sushantaryal05@gmail.com>
2025-09-14 21:12:58 -0500

    35

--- 3a/fig3_34.c
+++ 3a/fig3_34.c
@@ -5,3 +5,38 @@ Date: 2025-09-14
 Lab: Lab 3A (Figures 3.30–3.35)
 File: fig3_34.c
 */
+/**
+ * Figure 3.34
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid, pid1;
+
+    /* fork a child process */
+    pid = fork();
+
+    if (pid < 0) {
+        /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0) { /* child process */
+        pid1 = getpid();
+        printf("child: pid = %d\n", pid);   /* A */
+        printf("child: pid1 = %d\n", pid1); /* B */
+    }
+    else { /* parent process */
+        pid1 = getpid();
+        printf("parent: pid = %d\n", pid);   /* C */
+        printf("parent: pid1 = %d\n", pid1); /* D */
+        wait(NULL);
+    }
+
+    return 0;
+}
+
--- 3a/fig3_35.c
+++ 3a/fig3_35.c
@@ -5,3 +5,37 @@ Date: 2025-09-14
 Lab: Lab 3A (Figures 3.30–3.35)
 File: fig3_35.c
 */
+/**
+ * Figure 3.35
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#define SIZE 5
+
+int nums[SIZE] = {0, 1, 2, 3, 4};
+
+int main() {
+    int i;
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid == 0) { /* child process */
+        for (i = 0; i < SIZE; i++) {
+            nums[i] *= -i;
+            printf("CHILD: %d\n", nums[i]); /* LINE X */
+        }
+    } 
+    else if (pid > 0) { /* parent process */
+        wait(NULL);
+        for (i = 0; i < SIZE; i++) {
+            printf("PARENT: %d\n", nums[i]); /* LINE Y */
+        }
+    }
+
+    return 0;
+}
