25768d6ab5848dcbcb07171c2012aa06d7a8b63c
Sushant Aryal <sushantaryal05@gmail.com>
2025-09-14 21:11:16 -0500

    33

--- 3a/fig3_33.c
+++ 3a/fig3_33.c
@@ -5,3 +5,34 @@ Date: 2025-09-14
 Lab: Lab 3A (Figures 3.30–3.35)
 File: fig3_33.c
 */
+/**
+ * Figure 3.33
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    pid_t pid;
+
+    /* fork a child process */
+    pid = fork();
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0) { /* child process */
+        execlp("/bin/ls", "ls", NULL);
+        printf("LINE J");
+    }
+    else { /* parent process */
+        /* parent will wait for the child to complete */
+        wait(NULL);
+        printf("Child Complete");
+    }
+
+    return 0;
+}
